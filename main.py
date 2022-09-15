a1973e3a434cdc83cd095250ec1e8a529ef70f2c
KooshaG <75464454+KooshaG@users.noreply.github.com>
2022-09-15 13:14:03 -0400

    added function that sends a request to server with the slots of the day that is specified

--- main.py
+++ main.py
@@ -1,149 +1,156 @@
 from datetime import datetime, timedelta
+import requests
 
 RESERVATION_TIMES = [
-    {
-        'dow': 'Monday',
-        'iso_weekday': 1,
-        'startTime': '13:00:00',
-        'endTime': '16:00:00'
-    },
-    {
-        'dow': 'Friday',
-        'iso_weekday': 5,
-        'startTime': '14:30:00',
-        'endTime': '16:00:00'
-    },
-    {
-        'dow': 'Thursday',
-        'iso_weekday': 4,
-        'startTime': '15:00:00',
-        'endTime': '16:00:00'
-    }
+  {
+    'dow': 'Monday',
+    'iso_weekday': 1,
+    'startTime': '13:00:00',
+    'endTime': '16:00:00'
+  },
+  {
+    'dow': 'Friday',
+    'iso_weekday': 5,
+    'startTime': '14:30:00',
+    'endTime': '16:00:00'
+  },
+  {
+    'dow': 'Thursday',
+    'iso_weekday': 4,
+    'startTime': '15:00:00',
+    'endTime': '16:00:00'
+  }
 ]
 
 ROOMS = [ # first index is highest priority, as it goes down the list, the less favorable the spot is
-    [
-      {
-        'eid': 18520,
-        'tech': True,
-        'name': 'LB 257 - Croatia',
-        'priority': 1
-      },
-    ],
-    [
-      {
-        'eid': 18518,
-        'tech': False,
-        'name': 'LB 251 - Luxembourg',
-        'priority': 2
-      },
-      {
-        'eid': 18522,
-        'tech': False,
-        'name': 'LB 259 - New Zealand',
-        'priority': 2
-      },
-    ],
-    [
-      {
-        'eid': 18508,
-        'tech': True,
-        'name': 'LB 351 - Netherlands',
-        'priority': 3
-      },
-      {
-        'eid': 18535,
-        'tech': True,
-        'name': 'LB 353 - Kenya',
-        'priority': 3
-      },
-      {
-        'eid': 18536,
-        'tech': True,
-        'name': 'LB 359 - Vietnam',
-        'priority': 3
-      },
-    ],
-    [
-      {
-        'eid': 18529,
-        'tech': True,
-        'name': 'LB 311 - Haiti',
-        'priority': 4
-      },
-      {
-        'eid': 18530,
-        'tech': True,
-        'name': 'LB 316 - Australia',
-        'priority': 4
-      },
-      {
-        'eid': 18532,
-        'tech': True,
-        'name': 'LB 327 - Syria',
-        'priority': 4
-      },
-      {
-        'eid': 18533,
-        'tech': True,
-        'name': 'LB 328 - Zimbabwae',
-        'priority': 4
-      },
-    ],
-    [
-      {
-        'eid': 18510,
-        'tech': True,
-        'name': 'LB 451 - Brazil',
-        'priority': 5
-      },
-      {
-        'eid': 18512,
-        'tech': True,
-        'name': 'LB 453 - Japan',
-        'priority': 5
-      },
-      {
-        'eid': 18523,
-        'tech': True,
-        'name': 'LB 459 - Italy',
-        'priority': 5
-      },
-    ],
-    [
-      {
-        'eid': 18524,
-        'tech': True,
-        'name': 'LB 518 - Ukraine',
-        'priority': 6
-      },
-      {
-        'eid': 18525,
-        'tech': True,
-        'name': 'LB 520 - South Africa',
-        'priority': 6
-      },
-      {
-        'eid': 18526,
-        'tech': True,
-        'name': 'LB 522 - Peru',
-        'priority': 6
-      },
-      {
-        'eid': 18511,
-        'tech': True,
-        'name': 'LB 547 - Lithuania',
-        'priority': 6
-      },
-      {
-        'eid': 18528,
-        'tech': True,
-        'name': 'LB 583 - Poland',
-        'priority': 6
-      },
-    ]
+  [
+    {
+      'eid': 18520,
+      'tech': True,
+      'name': 'LB 257 - Croatia',
+      'priority': 1
+    },
+  ],
+  [
+    {
+      'eid': 18518,
+      'tech': False,
+      'name': 'LB 251 - Luxembourg',
+      'priority': 2
+    },
+    {
+      'eid': 18522,
+      'tech': False,
+      'name': 'LB 259 - New Zealand',
+      'priority': 2
+    },
+  ],
+  [
+    {
+      'eid': 18508,
+      'tech': True,
+      'name': 'LB 351 - Netherlands',
+      'priority': 3
+    },
+    {
+      'eid': 18535,
+      'tech': True,
+      'name': 'LB 353 - Kenya',
+      'priority': 3
+    },
+    {
+      'eid': 18536,
+      'tech': True,
+      'name': 'LB 359 - Vietnam',
+      'priority': 3
+    },
+  ],
+  # [ # some presentation rooms don't have tables, so we wont look at those
+  #   {
+  #     'eid': 18529,
+  #     'tech': True,
+  #     'name': 'LB 311 - Haiti',
+  #     'priority': 4
+  #   },
+  #   {
+  #     'eid': 18530,
+  #     'tech': True,
+  #     'name': 'LB 316 - Australia',
+  #     'priority': 4
+  #   },
+  #   {
+  #     'eid': 18532,
+  #     'tech': True,
+  #     'name': 'LB 327 - Syria',
+  #     'priority': 4
+  #   },
+  #   {
+  #     'eid': 18533,
+  #     'tech': True,
+  #     'name': 'LB 328 - Zimbabwae',
+  #     'priority': 4
+  #   },
+  # ],
+  [
+    {
+      'eid': 18510,
+      'tech': True,
+      'name': 'LB 451 - Brazil',
+      'priority': 5
+    },
+    {
+      'eid': 18512,
+      'tech': True,
+      'name': 'LB 453 - Japan',
+      'priority': 5
+    },
+    {
+      'eid': 18523,
+      'tech': True,
+      'name': 'LB 459 - Italy',
+      'priority': 5
+    },
+  ],
+  [
+    {
+      'eid': 18524,
+      'tech': True,
+      'name': 'LB 518 - Ukraine',
+      'priority': 6
+    },
+    {
+      'eid': 18525,
+      'tech': True,
+      'name': 'LB 520 - South Africa',
+      'priority': 6
+    },
+    {
+      'eid': 18526,
+      'tech': True,
+      'name': 'LB 522 - Peru',
+      'priority': 6
+    },
+    {
+      'eid': 18511,
+      'tech': True,
+      'name': 'LB 547 - Lithuania',
+      'priority': 6
+    },
+    {
+      'eid': 18528,
+      'tech': True,
+      'name': 'LB 583 - Poland',
+      'priority': 6
+    },
+  ]
 ]
 
+LID = 2161 # library id
+
+HEADERS = {
+  'User-Agent': 'Mozilla/5.0',
+  'Referer': 'https://concordiauniversity.libcal.com/reserve/webster'
+}
 
 def reservationDaysInTwoWeeksFromNow(day = RESERVATION_TIMES[0]):
   '''
@@ -178,11 +185,32 @@ def createDateStringsForRequest(date: datetime):
   startDate = date.strftime("%Y-%m-%d")
   endDate = nextDay.strftime("%Y-%m-%d")
   return startDate, endDate
+
+def getAvailabilityArray(startStr: str, endStr: str):
+  formData = {
+    'lid': LID,
+    'pageSize': 18,
+    'start': startStr,
+    'end': endStr
+  }
+  url = "https://concordiauniversity.libcal.com/spaces/availability/grid"
+  res = requests.post(url, headers=HEADERS, data=formData)
+  return res.json()
+ 
+def main(): 
+  for day in RESERVATION_TIMES:
+    print(f"Looking to reserve a room for the following {day['dow']}s")
+    reservationDates = reservationDaysInTwoWeeksFromNow(day)
+    for date in reservationDates:
+      print(datetime.ctime(date))
+      start, end = createDateStringsForRequest(date)
+      res = getAvailabilityArray(start, end)
+      print(res['slots'][:10])
+      
+  reservationDates = reservationDaysInTwoWeeksFromNow()
+  start, end = createDateStringsForRequest(reservationDates[0])
+  print("\n\n")
+  print(getAvailabilityArray(start, end)['slots'][:30])
   
-for day in RESERVATION_TIMES:
-  print(f"Looking to reserve a room for the following {day['dow']}s")
-  reservationDates = reservationDaysInTwoWeeksFromNow(day)
-  for date in reservationDates:
-    print(datetime.ctime(date))
-  for date in reservationDates:
-    print(createDateStringsForRequest(date))
+if __name__ == "__main__":
+  main()
