9f21a7469edaab5390330a3daeb536c5022c9cd9
KooshaG <75464454+KooshaG@users.noreply.github.com>
2022-09-26 11:22:17 -0400

    added something to test deployment

--- Reserve/__init__.py
+++ Reserve/__init__.py
@@ -14,4 +14,6 @@ def main(mytimer: func.TimerRequest) -> None:
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
     reserve.main()
-    logging.info('Python timer trigger function done')
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    logging.info('Python timer trigger function done at %s' % utc_timestamp)
