4c716b6dc08e4c034c040a6ab93baf9ef5983fb4
KooshaG <75464454+KooshaG@users.noreply.github.com>
2023-01-10 23:38:27 -0500

    Added room object

--- FlaskApp/db/__init__.py
+++ FlaskApp/db/__init__.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 
 base = declarative_base()
@@ -13,17 +13,35 @@ class User(base):
   reserved_dates = relationship("ReservedDate", back_populates="user", cascade="all, delete-orphan")
   reservation_requests = relationship("ReservationRequest", back_populates="user", cascade="all, delete-orphan")
   
+  def __repr__(self):
+    return f"ReservationRequest(id={self.id}, username='{self.username}', password={self.password})"
+  
 
-class ReservedDate(base):
-  __tablename__ = 'reserved_date'
+class Reservation(base):
+  __tablename__ = 'reservation'
   
   id = Column(Integer, primary_key=True, autoincrement=True)
   day_since_epoch = Column(Integer, nullable=False)
+  room_id = Column(Integer, ForeignKey("room.id"), nullable=False)
   
   user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
   user = relationship("User", back_populates="reserved_dates")
+  
+  room = relationship("Room", back_populates="room")
+  
+  
 
-
+class Room(base):
+  __tablename__ = 'room'
+  
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  name = Column(String(64), nullable=False)
+  tech = Column(Boolean, nullable=False)
+  priority = Column(Integer, nullable=False)
+  seats = Column(Boolean, nullable=False)
+  
+  reservations = relationship("Reservation", back_populates="reservation")
+  
 class ReservationRequest(base):
   __tablename__ = 'reservation_requests'
   
@@ -40,4 +58,4 @@ class ReservationRequest(base):
 
   
   def __repr__(self):
-    return f"ReservationRequest(dow='{self.dow}', iso_weekday={self.iso_weekday}, startTime='{self.startTime}', endTime='{self.endTime}', slots30mins={self.slots30mins})"
+    return f"ReservationRequest(id={self.id}, dow='{self.dow}', iso_weekday={self.iso_weekday}, startTime='{self.startTime}', endTime='{self.endTime}', slots30mins={self.slots30mins}, user_id={self.user_id})"
